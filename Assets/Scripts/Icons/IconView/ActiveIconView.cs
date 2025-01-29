18b0207550403821d9a99a13fa882887f45aaf90
antennaria <antennaria.arktik@gmail.com>
2025-01-29 20:45:36 +0300

    change bag

--- Assets/Scripts/Icons/IconView/ActiveIconView.cs
+++ Assets/Scripts/Icons/IconView/ActiveIconView.cs
@@ -4,14 +4,8 @@ using System;
 
 public class ActiveIconView : MonoBehaviour
 {
-    [SerializeField] private IconView _currentIcon;
+    [SerializeField] static private IconView _currentIcon;
 
-    public void Update()
-    {
-        if (_currentIcon != null)
-            Debug.Log("wwww" + _currentIcon.name);
-
-    }
     public void ChangeActiveIcon(IconView icon)
     {
         if (_currentIcon != null)
@@ -21,8 +15,6 @@ public class ActiveIconView : MonoBehaviour
 
         _currentIcon = icon;
         _currentIcon.ChangeActivePanelWhenClick(true);
-        Debug.Log(_currentIcon == null);
-
     }
 
     public void ResetActive()
